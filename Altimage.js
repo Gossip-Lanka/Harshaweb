da86c4447fba98b1abdb884acc1e35869adfac6e
Gossip-Lanka <63828178+Gossip-Lanka@users.noreply.github.com>
2020-05-12 10:06:37 +0300

    Update Altimage.js

--- Altimage.js
+++ Altimage.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 $('img').each(function(){     
 	var $img = $(this);     
 	var filename = $img.attr('src')     
-  //$img.attr('title', filename.substring((filename.lastIndexOf('/'))+1, filename.lastIndexOf('.')));     
+  	$img.attr('class', 'b-lazy');     
 	$img.attr('alt', filename.substring((filename.lastIndexOf('/'))+1, filename.lastIndexOf('.')));   	
 		}); 
 }); 
